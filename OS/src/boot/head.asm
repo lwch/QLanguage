1d869288adde40acf5acb0164401737128da2c23
lwch748 <lwch748@6d982bb3-a5a7-393d-ffc5-582415be9c85>
2012-11-04 17:16:48 +0000

    增加字母H的显示
    git-svn-id: http://qlanguage.googlecode.com/svn/trunk@34 6d982bb3-a5a7-393d-ffc5-582415be9c85

--- OS/src/boot/head.asm
+++ OS/src/boot/head.asm
@@ -4,17 +4,19 @@
 .text
 	jmp LABEL_BEGIN
 
-LABEL_GDT:         Descriptor 0,       0,      0
-LABEL_DESC_CODE32: Descriptor 0,       0xFFFF, (DA_C + DA_32)
-LABEL_DESC_VIDEO:  Descriptor 0xB8000, 0xFFFF, DA_DRW
+LABEL_GDT:         Descriptor 0,       0,          0
+LABEL_DESC_DATA:   Descriptor 0,       0xFFFFFFFF, DA_DRW
+LABEL_DESC_CODE32: Descriptor 0,       0xFFFF,     (DA_CR + DA_32)
+LABEL_DESC_KERNEL: Descriptor 0x12000, 0xFFFF,     (DA_CR + DA_32)
 
 .set GdtLen, (. - LABEL_GDT)
 
 GdtPtr: .short (GdtLen - 1)
         .int   0
 
+.set SelectorData,   (LABEL_DESC_DATA   - LABEL_GDT)
 .set SelectorCode32, (LABEL_DESC_CODE32 - LABEL_GDT)
-.set SelectorVideo,  (LABEL_DESC_VIDEO  - LABEL_GDT)
+.set SelectorKernel, (LABEL_DESC_KERNEL - LABEL_GDT)
 
 LABEL_BEGIN:
 	mov %cs, %ax
@@ -23,6 +25,10 @@ LABEL_BEGIN:
 	mov %ax, %ss
 	
 	mov $0, %sp
+	
+	mov $0x13, %al
+	mov $0, %ah
+	int $0x10
 	
 	xor  %eax, %eax
 	mov  %cs, %ax
@@ -46,7 +52,7 @@ LABEL_BEGIN:
 	inb  $0x92, %al
 	orb  $0b00000010, %al
 	outb %al, $0x92
-	
+
 	movl %cr0, %eax
 	orl  $1, %eax
 	movl %eax, %cr0
@@ -55,4 +61,27 @@ LABEL_BEGIN:
 
 LABEL_CODE32:
 .code32
-	mov $10000, %esp
+	mov $SelectorData, %ax
+	mov %ax, %ds
+	mov %ax, %es
+	mov %ax, %fs
+	mov %ax, %gs
+	mov %ax, %ss
+
+	mov $0x20000, %esp
+	
+	mov $(init + 0x10000), %esi
+	mov $0x12000, %edi
+	mov $1280, %ecx
+	call memcpy
+	
+	ljmpl $SelectorKernel, $0x0
+memcpy:
+	mov (%esi), %eax
+	add $4, %esi
+	mov %eax, (%edi)
+	add $4, %edi
+	sub $1, %ecx
+	jnz memcpy
+	ret
+init:
