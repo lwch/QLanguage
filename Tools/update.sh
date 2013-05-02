de3e6e0e2ccb734559dc0a770f8fe6d872f5335b
lwch748@gmail.com <lwch748@gmail.com@6d982bb3-a5a7-393d-ffc5-582415be9c85>
2013-05-02 09:48:18 +0000

    修改脚本
    git-svn-id: http://qlanguage.googlecode.com/svn/trunk@147 6d982bb3-a5a7-393d-ffc5-582415be9c85

--- Tools/update.sh
+++ Tools/update.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
+
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
 MAILADDR="lwch748@gmail.com"
 CURRENT_PATH=/opt/service
-svn up ~/QLanguage
-cmake ~/QLanguage/Source -G "Unix Makefiles" > $CURRENT_PATH/cmake.txt
-cd ~/QLanguage/Source && make 2> $CURRENT_PATH/make.txt
-cd ~/QLanguage/Source/QCoreUnitTest && ./QCoreUnitTest < $CURRENT_PATH/input.txt > $CURRENT_PATH/unittest.txt
+SOURCE_PATH="http://qlanguage.googlecode.com/svn/trunk/"
+EXPORT_PATH=~/QLanguage/
+CMAKE_PATH=$EXPORT_PATH/./Source
+UNITTEST_PATH=$CMAKE_PATH/./QCoreUnitTest
+UNITTEST_EXEC=QCoreUnitTest
+
+if [ -d "$EXPORT_PATH" ]; then
+  rm -fr $EXPORT_PATH
+fi
+svn export $SOURCE_PATH $EXPORT_PATH
+cd $CMAKE_PATH && cmake -G "Unix Makefiles" > $CURRENT_PATH/cmake.txt
+cd $CMAKE_PATH && make 2> $CURRENT_PATH/make.txt
+$UNITTEST_PATH/$UNITTEST_EXEC < $CURRENT_PATH/input.txt > $CURRENT_PATH/unittest.txt
 mail -s "QLanguageUpdate on time $DATE(CMake Report)" $MAILADDR < $CURRENT_PATH/cmake.txt
 mail -s "QLanguageUpdate on time $DATE(Make Report)" $MAILADDR < $CURRENT_PATH/make.txt
 mail -s "QLanguageUpdate on time $DATE(UnitTest Report)" $MAILADDR < $CURRENT_PATH/unittest.txt
