23f9eedd63f3c06c5f9a8cc9159cc182614e355f
lwch748@gmail.com <lwch748@gmail.com@6d982bb3-a5a7-393d-ffc5-582415be9c85>
2013-05-07 06:19:11 +0000

    修改Update脚本
    git-svn-id: http://qlanguage.googlecode.com/svn/trunk@163 6d982bb3-a5a7-393d-ffc5-582415be9c85

--- Tools/update.sh
+++ Tools/update.sh
@@ -8,6 +8,8 @@ EXPORT_PATH=~/QLanguage/
 CMAKE_PATH=$EXPORT_PATH/./Source
 UNITTEST_PATH=$CMAKE_PATH/./QCoreUnitTest
 UNITTEST_EXEC=QCoreUnitTest
+GITHUB_PATH=~/QLanguage_GitHub/
+GITHUB_URL=git@github.com:lwch/QLanguage.git
 
 if [ -d "$EXPORT_PATH" ]; then
   rm -fr $EXPORT_PATH
@@ -19,3 +21,12 @@ $UNITTEST_PATH/$UNITTEST_EXEC < $CURRENT_PATH/input.txt > $CURRENT_PATH/unittest
 mail -s "QLanguageUpdate on time $DATE(CMake Report)" $MAILADDR < $CURRENT_PATH/cmake.txt
 mail -s "QLanguageUpdate on time $DATE(Make Report)" $MAILADDR < $CURRENT_PATH/make.txt
 mail -s "QLanguageUpdate on time $DATE(UnitTest Report)" $MAILADDR < $CURRENT_PATH/unittest.txt
+
+# GITHUB
+# first run git "svn clone" and "git remote add"
+#git svn clone $SOURCE_PATH $GITHUB_PATH
+cd $GITHUB_PATH
+#git remote add origin $GITHUB_URL
+git svn rebase
+git push -f > $CURRENT_PATH/git_push.txt
+mail -s "GitHub push on time $DATE" $MAILADDR < $CURRENT_PATH/git_push.txt
